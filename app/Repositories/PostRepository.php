a88985e9847ff581a5e0ac5dfe92704b787cd43f
Ryan <lcrob55@gmail.com>
2020-01-20 00:31:35 +0800

    feat:Post Repository相關測試-實作 lastest10 方法 issue #2

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -16,20 +16,29 @@ class PostRepository
     {
         return Post::with('user')->find($id);
     }
-	
+
 	 public function delete($id)
     {
         return Post::destroy($id);
     }
-	
+
 	 public function create(array $data)
     {
         return auth()->user()->posts()->create($data);
     }
-	
+
     public function update($id, array $data)
     {
         $post = Post::find($id);
         return $post ? $post->update($data) : false;
+    }
+
+    /**
+     * 取得最新10筆文章
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function latest10()
+    {
+        return Post::query()->orderBy('id', 'desc')->limit(10)->get();
     }
 }
